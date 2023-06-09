9dd70a8e16cd6366a642c8603b5a5afe0b0bcc51
Nikita Olechko <nikita.olechko@gmail.com>
2023-06-09 15:40:29 -0700

    setup stylesheet and folders

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ require('dotenv').config();
 const express = require('express');
 
 app.set('view engine', 'ejs');
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
