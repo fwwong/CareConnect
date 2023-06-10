fe694729a85dd547696c7afb68772abe9ea5ee34
Fiona Wong <fwwong01@gmail.com>
2023-06-10 16:46:06 -0700

    Added a usaage for middleware in index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(middleware);
 require('./routes/home')(app);
 
 const port = process.env.PORT || 3000;
