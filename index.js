2897302e850983498bd1054c934b76c987867c0d
Nikita Olechko <nikita.olechko@gmail.com>
2023-06-09 17:32:02 -0700

    Create route for API call

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+app.use(express.json());
 
 
 
--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,26 @@
 var express = require('express');
 router = express.Router();
+const makeAPIRequest = require('./OpenAIcall');
 
 
 module.exports = function (app) {
+
+    function createGPTPrompt(userPrompt){
+        return (userPrompt)
+    }
+
     app.get('/', (req, res) => {
         // render the home page
-        res.render('home');
+        res.render('home', {gptPrompt: false});
+    });
+
+    app.post('/submitPrompt', async (req, res) => {
+        var userPrompt = req.body.prompt;
+        console.log(userPrompt)
+        return
+        // modify the prompt to be used in the API call
+        var fullPrompt = createGPTPrompt(userPrompt);
+        var gptResponse = await makeAPIRequest(fullPrompt);
+        res.render('home', {gptPrompt: gptResponse})
     });
 };
